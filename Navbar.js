bef02bc7f21257e100acebd12ab705c581799684
altinaibrahimi <altinaibrahimi123@gmail.com>
2022-11-09 19:55:42 +0100

    JS

---
+++ Navbar.js
@@ -0,0 +1,33 @@
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+    const el = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
+
